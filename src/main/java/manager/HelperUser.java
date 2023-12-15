1a4daa2dc311104154a83d240c44c224dfdac165
Kaufman17 <kaufman.ua@gmail.com>
2023-12-15 20:06:33 +0200

    Hw_loginSuccessful

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,27 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLoginRegistrationForm(){
+        click(By.cssSelector("a.navigation-link[ng-reflect-router-link='login']"));
+    }
+
+    public void fillLoginRegistrationForm(String email, String password) {
+        type(By.id("email"),email);
+        type(By.cssSelector("[ng-reflect-name='password']"), password);
+    }
+
+    public void submitLogin(){
+        click(By.cssSelector("button[type='submit']"));
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//button[text()='Ok']"));
     }
 }
